8ff7b0fd7fe94633a89825a68675affb6662a498
Joponspolo <159774189+jonathanPons@users.noreply.github.com>
2025-12-18 17:50:01 +0100

    dynamiqueJSON 18/12

--- index.js
+++ index.js
@@ -2,43 +2,68 @@ const daily = document.querySelector(".daily");
 const Weekly = document.querySelector(".Weekly");
 const Monthly = document.querySelector(".Monthly");
 
+let data;
+
 async function getData() {
-  try {
-    const response = await fetch("./data.json");
-    console.log(response);
+  // Aller chercher le fichier JSON
+  const response = await fetch("./data.json");
+  console.log(response);
 
-    if (!response.ok) {
-      throw new Error("Error");
-    }
-    const data = await response.json();
-    console.log(data);
-
-    data.forEach((element) => {
-      const title = element.title;
-
-      console.log("Element:", element);
-      console.log(title);
-      const titleLower = title.toLowerCase().replace(/ /g, "");
-      console.log(titleLower);
-
-      const card = document.querySelector(`#${titleLower}`);
-      console.log("Card trouvé:", card);
-
-      const mainHour = card.querySelector(".main-hour");
-      console.log(mainHour);
-
-      const previousHour = card.querySelector(".previous-hour");
-      console.log(previousHour);
-
-      const current = element.timeframes.daily.current;
-      const previous = element.timeframes.daily.previous;
-      console.log(current);
-      mainHour.textContent = `${current} Hrs`;
-      previous.textContent = ` Last Week : ${previous}Hrs`;
-    });
-  } catch (error) {
-    console.log("Erreur", error);
+  if (!response.ok) {
+    return "Error";
   }
+  // l'afficher , le rendre visible
+  data = await response.json();
+  changeData("weekly");
+  console.log(data);
+}
+getData();
+
+// Creer une function de changement de periode
+
+// click sur la periode
+function changeData(periode) {
+  console.log("fonction appelée avec : ", periode);
+  data.forEach((element) => {
+    //recuperer le titre de chacun
+    const title = element.title;
+    console.log(title);
+    // Je veux que le title de chaque objet fasse apparaitre ses heures dans les
+    //  DIV correspondantes
+    let previousText;
+    const LowerTitle = title.toLowerCase().replace(/ /g, "");
+    const card = document.querySelector(`#${LowerTitle}`);
+    console.log(card);
+    const currentHour = element.timeframes[periode].current;
+    console.log(currentHour);
+    const previousHour = element.timeframes[periode].previous;
+    console.log(previousHour);
+
+    const mainHour = card.querySelector(".main-hour");
+
+    const lastHour = card.querySelector(".previous-hour");
+
+    mainHour.textContent = `${currentHour} hrs`;
+    console.log(mainHour);
+
+    if (periode === "daily") {
+      previousText = "Yerterday";
+    } else if (periode === "weekly") {
+      previousText = "Last Week";
+    } else {
+      previousText = "Last Month";
+    }
+
+    lastHour.textContent = ` ${previousText} - ${previousHour} hrs`;
+  });
 }
 
-getData();
+daily.addEventListener("click", () => {
+  changeData("daily");
+});
+Weekly.addEventListener("click", () => {
+  changeData("weekly");
+});
+Monthly.addEventListener("click", () => {
+  changeData("monthly");
+});
